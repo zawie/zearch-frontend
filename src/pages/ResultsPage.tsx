afab44bd1c28eb0def6538108f072f7860424a92
zawie <zawie@rice.edu>
2022-12-30 12:06:51 -0800

    loading & pagination

--- src/pages/ResultsPage.tsx
+++ src/pages/ResultsPage.tsx
@@ -1,10 +1,15 @@
-import { Input, Space, Pagination} from 'antd';
+import { Input, Space, Pagination, Spin} from 'antd';
 import ResultCard from '../components/result/Result'
 import './ResultsPage.css'
 
 import { Result } from '../helpers/engineInterface'
 
+import { LoadingOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+
+const antIcon = <LoadingOutlined style={{ fontSize: 64 }} spin />;
 const { Search } = Input;
+
 
 type ResultsPageProps = {
   onSearch: (query: string) => void
@@ -14,7 +19,10 @@ type ResultsPageProps = {
 }
 
 const ResultsPage = ({onSearch, query, results, loading}: ResultsPageProps) => {
-    return <div style={{padding:15}}>
+    const PAGE_SIZE = 10;
+    const [page, setPage] = useState(0);
+
+return <div style={{padding:15}}>
         <Search
           enterButton="Search"
           size="large"
@@ -24,11 +32,28 @@ const ResultsPage = ({onSearch, query, results, loading}: ResultsPageProps) => {
         <br/>
         <br/>
 
-        <div className="ResultsList">
-          {results.map((r: Result) => <ResultCard result={r}/>)}
-        </div>
-        <br/>
-        <Pagination defaultCurrent={2} total={40} pageSize={16} />
+        {loading && <>
+          <br/>
+          <br/>
+          <br/>
+          <Spin indicator={antIcon} />
+        </>}
+
+        {(!loading && results.length > 0) && <>
+          <div className="ResultsList">
+            {results.slice(Math.min(page*PAGE_SIZE, results.length-1), Math.min((page+1)*PAGE_SIZE, results.length)).map((r: Result) => <ResultCard result={r}/>)}
+          </div>
+          <br/>
+          <Pagination 
+            total={results.length} 
+            defaultCurrent={page+1}
+            pageSize={PAGE_SIZE} 
+            onChange={(page,_) => {
+              setPage(page-1);
+            }}
+          />
+        </>}
+        
       <Space/>
     </div>;
   };

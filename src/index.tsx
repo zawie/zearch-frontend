fd39e3ba2dff4aa80f36d3404b2fc60359a6c1d4
zawie <zawie@rice.edu>
2022-12-31 11:30:36 -0800

    remove unused import

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
-import { purple } from '@ant-design/colors';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
